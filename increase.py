29540b530880de2301a0b76f44fefc7d457b28ae
Paupar <64657169+g-arkantos@users.noreply.github.com>
2020-06-19 21:09:04 +0530

    Update increase.py

--- increase.py
+++ increase.py
@@ -1,3 +1,6 @@
+'''
+a program that takes number n as an input parameter and prints n-digit strictly increasing numbers
+'''
 def increase(n,l=[]):
 	if n==0:
 		return 0
@@ -7,13 +10,8 @@ def increase(n,l=[]):
 		(increase(n-1))
 		l.append(n)
 		return l		
-
 def main():
 	num=int(input('Enter a number: '))
 	print(increase(num))
-	
-
 if __name__=='__main__':
 		main()	
-
-		
