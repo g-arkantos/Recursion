f121c6d747288b1060ffd43b0d7c62fce90867cf
Paupar <64657169+g-arkantos@users.noreply.github.com>
2020-06-19 21:06:29 +0530

    Add files via upload

---
+++ increase.py
@@ -0,0 +1,19 @@
+def increase(n,l=[]):
+	if n==0:
+		return 0
+	elif n==1:
+		return l.append(1)
+	else:
+		(increase(n-1))
+		l.append(n)
+		return l		
+
+def main():
+	num=int(input('Enter a number: '))
+	print(increase(num))
+	
+
+if __name__=='__main__':
+		main()	
+
+		
